fa437172d96cd15f72559cbcf55617f62f3eeccf
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-11 20:34:56 -0300

    modulos

---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Thiago", 36));
+console.log(mostraCidade("Thiago", 'João Pessoa'));
+console.log(mostraHobby("Thiago", 'jogar videogame'));
