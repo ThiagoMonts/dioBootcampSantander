d0881c047f8f2b8a4aafd3b791bccf389579e34a
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-13 20:34:25 -0300

    Primeiro commit Angular

---
+++ Angular/course-manager/src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: string;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+}
