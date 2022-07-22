22a5e393a4e47cb9e28f7c9eced0843b60a0314f
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-22 17:45:59 -0300

    CRUD

--- Angular/crud-dio-angular/src/app/app.module.ts
+++ Angular/crud-dio-angular/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UsersListComponent,
+    UserFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/crud-dio-angular/src/app/pages/users/users-list/users-list.component.ts
+++ Angular/crud-dio-angular/src/app/pages/users/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
+  users: Array<User> = [];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    }, (err) => {
+      console.log('ERRO AO EXECUTAR', err);
+    })
+  }
+
+  deleteUser(id: number): void {
+    this.userService.deleteUser(id).subscribe(response => {
+      console.log('Usuário Excluído!')
+    }, (err) => {
+      console.log(err)
+    }, () => {
+      this.getUsers();
+    })
   }
 
 }
