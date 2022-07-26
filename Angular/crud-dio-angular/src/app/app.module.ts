5b1f520020cfca1ff2e5bbf6ebe7c2fab01ac0d1
ThiagoMonts <thiagohonorato@hotmail.com>
2022-07-26 18:06:32 -0300

    Criação de Pipes com Angular

--- Angular/crud-dio-angular/src/app/app.module.ts
+++ Angular/crud-dio-angular/src/app/app.module.ts
@@ -19,7 +19,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { TodoItemComponent } from './pages/todo/todo-item/todo-item.component';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component'
+import { UpperCasePipe } from '@angular/common';
+import { PrintListPipe } from './print-list.pipe';
 
 @NgModule({
   declarations: [
@@ -32,6 +35,8 @@ import { MatIconModule } from '@angular/material/icon'
     LifeCycleComponent,
     TodoListComponent,
     TodoItemComponent,
+    PipesExampleComponent,
+    PrintListPipe,
 
   ],
   imports: [
@@ -49,7 +54,7 @@ import { MatIconModule } from '@angular/material/icon'
     MatButtonModule,
     MatIconModule,
   ],
-  providers: [],
+  providers: [UpperCasePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
